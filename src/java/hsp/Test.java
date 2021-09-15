5dd73ed74e8ac0dbe969f1586485e21ba40eedda
huntiandi <1413897484@qq.com>
2021-09-15 16:34:30 +0800

    0915Company

--- src/java/hsp/Test.java
+++ src/java/hsp/Test.java
@@ -7,9 +7,9 @@ package hsp;
  * @data: 2021/9/7 22:32
  */
 public class Test {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         for (int i = 0; i < 8; i++) {
-            if (i<4) {
+            if (i < 4) {
                 for (int j = 0; j < 5 - i; j++) {
                     System.out.print(" ");
                 }
@@ -17,11 +17,11 @@ public class Test {
                     System.out.print("*");
                 }
                 System.out.println("");
-            }else {
-                for (int j = 0; j <i-2; j++) {
+            } else {
+                for (int j = 0; j < i - 2; j++) {
                     System.out.print(" ");
                 }
-                for (int j = 0; j < 16-(2*i+1); j++) {
+                for (int j = 0; j < 16 - (2 * i + 1); j++) {
                     System.out.print("*");
                 }
                 System.out.println("");
