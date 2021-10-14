2b79d41d763757680f27fc974a5221ffbe54bc85
yang <1413897484@qq.com>
2021-10-15 01:11:36 +0800

    20211014Home

--- src/com/file/FileInput_.java
+++ src/com/file/FileInput_.java
@@ -3,23 +3,23 @@ package com.file;
 import com.hsp.collection.DoubleLinked;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @ProjectName: com.file
  * @author: ZhangBiBo
- * @description:
+ * @description: file的输入输出流
  * @data: 2021/10/14
  */
 public class FileInput_ {
     public static void main(String[] args) {
         FileInputStream fileInputStream =null;
         int read = 0;
+        String filePath = "D:\\new1.txt";
+        File file = new File(filePath);
         try {
-             fileInputStream = new FileInputStream("d:\\new1.txt");
+                file.createNewFile();
+             fileInputStream = new FileInputStream(filePath);
             while ((read= fileInputStream.read())!=-1){
                 System.out.print((char) read);
             }
@@ -64,6 +64,9 @@ public class FileInput_ {
         FileOutputStream foo = null;
         try {
             fss = new FileInputStream("d:\\new1.txt");
+//            foo = new FileOutputStream("d:\\new2.txt");
+            //添加true的意思就是在文本的末尾添加文本，而不是覆盖已有的文本
+            //加入流并未关闭，还是原来的流，不会覆盖源文本，会拿几个写几个，以此提高效率
             foo = new FileOutputStream("d:\\new2.txt");
             byte[] bt = new byte[2];
             int temp = -1;
@@ -76,6 +79,27 @@ public class FileInput_ {
             try {
                 fss.close();
                 foo.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void out(){
+
+        String path = "d:\\new1.txt";
+        FileOutputStream fileOutputStream =null;
+        try {
+            String txt = "zhangyang";
+            fileOutputStream = new FileOutputStream(path,true);
+            //如果只是字符串全部写进去直接string.getBytes()
+            fileOutputStream.write(txt.getBytes(),0,txt.length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileOutputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
