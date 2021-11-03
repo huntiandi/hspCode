1d8bdaa0f86eaad1aa59183443039ceb2da9ed6d
huntiandi <1413897484@qq.com>
2021-11-03 18:18:13 +0800

    1103Company2

---
+++ src/com/hsp/java8/TimeAPI.java
@@ -0,0 +1,17 @@
+package com.hsp.java8;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * @ProjectName: com.hsp.java8
+ * @author: ZhangBiBo
+ * @description: 新日期API
+ * @data: 2021/11/3
+ */
+public class TimeAPI {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
