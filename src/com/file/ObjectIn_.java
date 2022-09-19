ff29ab279b16b4df17f0b450f4f96999f33ce4f2
yang <1413897484@qq.com>
2022-09-19 09:45:42 +0800

    20220919-Home-面试题之序列化

--- src/com/file/ObjectIn_.java
+++ src/com/file/ObjectIn_.java
@@ -32,3 +32,64 @@ public class ObjectIn_ {
         ois.close();
     }
 }
+
+class Test08 {
+    public static void main(String[] args) {
+        try (
+                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("object.txt"));
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream("object.txt"))) {
+            Account account = new Account();
+            Account account0 = new Account();
+            account.setPassword("123456");
+            account.setId(2);
+
+            oos.writeObject(account);
+            oos.flush();
+            account0.setPassword("456789");
+            account0.setId(3);
+            oos.writeObject(account0);
+            //从文件依次读出两个对象
+            Account account1 = (Account) ois.readObject();
+            Account account2 = (Account) ois.readObject();
+            //因为父类没有实现Serializable接口，所以反序列化就是新建了，那么就是默认值了
+            System.out.println(account1.getPassword()+"---"+account1.getId());
+            System.out.println(account2.getPassword()+"---"+account2.getId());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Account extends Base implements Serializable {
+    private String password;
+
+    private static String sex="man";
+
+    public static String getSex() {
+        return sex;
+    }
+
+    public static void setSex(String sex) {
+        Account.sex = sex;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+class Base{
+    private int id ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
